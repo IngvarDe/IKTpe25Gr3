64428073d746c3279ae6303b5598ddd448a707c0
MSI\ingva <inde999@gmail.com>
2025-10-03 11:49:25 +0300

    IfAndElsemethodCall2Flowchart

---
+++ 05. 03.10.25/IfAndElseMethodCall2/IfAndElseMethodCall2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace IfAndElseMethodCall2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //tuleb teha if ja else-ga paaris ja paaritu arvu kontroll,
+            //mis kutsub välja meetodi
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number %2 == 0)
+            {
+                EvenNumber();
+            }
+            else
+            {
+                OddNumber();
+            }
+
+
+            //tuleb teha meetod EvenNumber()
+            //Kui kutsutakse välja see meetod, siis tuleb vastus:
+            //Tegemist on paaris numbriga
+
+            //tuleb teha teine meetod OddNumber()
+            //Kui kutsutakse välja see meetod, siis tuleb vastus:
+            //Tegemist on paaritu numbriga
+        }
+
+        static void EvenNumber()
+        {
+            Console.WriteLine("See on paaris number");
+        }
+
+        static void OddNumber()
+        {
+            Console.WriteLine("See on paaritu number");
+        }
+    }
+}
