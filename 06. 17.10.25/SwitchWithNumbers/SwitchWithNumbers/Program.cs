c58fed0e9c131ed027e8a90501522bde6361003f
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-17 11:22:39 +0300

    SwitchWitNumbers

---
+++ 06. 17.10.25/SwitchWithNumbers/SwitchWithNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SwitchWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Teie töö on teha switch rakendus,
+            //kus on kolm case (1, 2, 3).
+            //Iga case puhul teeb erinev arv beep-e
+
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    Console.Beep(1000, 500);
+                    break;
+                case 2:
+                    Console.Beep(2000, 500);
+                    Console.Beep(2000, 500);
+                    break;
+                case 3:
+                    Console.Beep(3000, 50);
+                    Console.Beep(3000, 50);
+                    Console.Beep(3000, 50);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
+    }
+}
