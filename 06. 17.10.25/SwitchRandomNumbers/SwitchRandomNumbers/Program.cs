21e988f2ee0e8dafe2ef1a0bd58efb995e10da25
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-17 10:30:09 +0300

    SwitchRandomNumber

---
+++ 06. 17.10.25/SwitchRandomNumbers/SwitchRandomNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SwitchRandomNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Genereerib numbreid ühest kuni kuueni");
+
+            //kõik kuus numbrit tuleb ära käsitleda switchiga
+            int cube = new Random().Next(1, 7);
+
+            switch (cube)
+            {
+
+                case 1:
+                    Console.WriteLine("Tuli 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Tuli 2");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Tuli 3");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Tuli 4");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Tuli 5");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Tuli 6");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
