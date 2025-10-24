511a2c0bed38f371a3d283bdeabe149494043e70
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-24 11:43:28 +0300

    SwitchMultiple

---
+++ 07. 24.10.25/MultipleSwitch/MultipleSwitch/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MultipleSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiple cases");
+
+            string topic, category;
+
+            topic = Console.ReadLine();
+
+            switch (topic)
+            {
+                case "Into to C#":
+                case "Variables":
+                case "Data Types":
+                    category = "Basics";
+                    break;
+
+                case "Loops":
+                case "If statements":
+                case "Jump statements":
+                    category = "Control Flow";
+                    break;
+
+                case "Class & Objects":
+                case "Inheritance":
+                case "Constructors":
+                    category = "OOP-s Concept";
+                    break;
+
+                default:
+                    category = "Unknown";
+                    break;
+            }
+            Console.WriteLine("Category is " + category);
+        }
+    }
+}
