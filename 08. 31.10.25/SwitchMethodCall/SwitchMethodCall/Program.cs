6a729b20dbe0f0b97f0b3fbfaec724c123fcdfd3
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-10-31 11:22:26 +0200

    SwitchMethodCall

---
+++ 08. 31.10.25/SwitchMethodCall/SwitchMethodCall/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SwitchMethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //teha switch rakendus,
+            //kus kasutaja sisestab sõiduki tüübi (coupe või truck)
+            string input = Console.ReadLine();
+            //ja programm kutsub vastava meetodi (CarCoupe või Truck)
+            switch (input)
+            {
+                case "Car Coupe":
+                    //meetodi kutse CarCoupe
+                    CarCoupe();
+                    break;
+
+                case "Truck":
+                    //meetodi kutse Truck
+                    Truck();
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown vehicle type.");
+                    break;
+            }
+
+        }
+
+        static void CarCoupe()
+        {
+            Console.WriteLine("This is a coupe car.");
+        }
+
+        static void Truck()
+        {
+            Console.WriteLine("This is a truck.");
+        }
+    }
+}
