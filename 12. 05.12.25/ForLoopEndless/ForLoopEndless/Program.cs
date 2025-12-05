2cc7748c6dfd0d287e3e6e122a92dc3cbe2195df
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-12-05 10:16:33 +0200

    ForLoopEndless

---
+++ 12. 05.12.25/ForLoopEndless/ForLoopEndless/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForLoopEndless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ushort i = 1;
+
+            //kui on 10 rida konsoolis, siis lõpetab ära
+            //kasutada break;
+            for (; ; )
+            {
+                Console.WriteLine("i väärtus on " + i);
+                i++;
+
+                if (i > 10)
+                {
+                    break;
+                }    
+            }
+        }
+    }
+}
