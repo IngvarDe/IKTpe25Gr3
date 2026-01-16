dde952a37812e9090603a16612a123aaecd51808
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-01-16 10:43:42 +0200

    FileCreate

---
+++ 19. 16.01.26/FileCreate/FileCreate/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FileCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjutab faili läbi konsooli");
+
+            string filePath = "C:/Users/ingva/Desktop/WriteToFile.txt";
+            string input = Console.ReadLine();
+
+            File.WriteAllText(filePath, input);
+        }
+    }
+}
